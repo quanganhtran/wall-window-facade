de833a617712e9acca3ff9c6b6654966a4b6af7d
quanganhtran <quanganhtran@users.noreply.github.com>
2022-08-06 05:19:09 +0300

    Update base path

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withTM = require('next-transpile-modules')(['immer']);
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: '/wall-window-facade',
 }
 
 module.exports = withTM(nextConfig);
